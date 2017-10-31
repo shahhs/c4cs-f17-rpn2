2acfa6f9c13128196aa466ba28fa2c7697dd6319
shahhs <shahhs@umich.edu>
2017-10-31 11:20:20 -0400

    added exponentiation test case

--- test_rpn.py
+++ test_rpn.py
@@ -6,3 +6,7 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+	
+	def test_exponentiation(self):
+		result = rpn.calculate("3 4 ^")
+		self.assertEqual(81, result)
